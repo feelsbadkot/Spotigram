ae7571c963f2a5ea54e8516b2afffaa5f0f45058
feelsbadkot <bond1010a@gmail.com>
2021-04-11 22:30:04 +0500

    добавил поиск по альбому

--- spotify_functions.py
+++ spotify_functions.py
@@ -25,18 +25,25 @@ def search_for_track(text: str):
         year = elem['album']['release_date'][:4]
         name = elem['name']
         album = elem['album']['name']
+        minutes = elem["duration_ms"] // 60000
+        if minutes < 10:
+            minutes = '0' + str(minutes)
+        seconds = elem["duration_ms"] % 60000 // 1000
+        if seconds < 10:
+            seconds = '0' + str(seconds)
+        duration = f'{minutes}:{seconds}'
         link = elem['external_urls']['spotify']
         if album != name:
-            result = f'{index}. {artist} - {name} ({album}, {year})\n{link}'
+            result = f'{index}. {artist} - {name} ({album}, {year}) - {duration}\n{link}'
         else:
-            result = f'{index}. {artist} - {name} ({year})\n{link}'
+            result = f'{index}. {artist} - {name} ({year}) - {duration}\n{link}'
         total.append(result)
     if len(total) == 1:
         total[0] = total[0][3:]
     return total
 
 
-def search_for_artist(text):
+def search_for_artist(text: str):
     s = ' '.join(text.split())
     res = spoti.search(q=s, limit=5, type='artist', market='RU')
     total = []
@@ -59,13 +66,34 @@ def search_for_artist(text):
     return total
 
 
-def search_for_album(text):
+def search_for_album(text: str):
+    s = ' '.join(text.split())
+    res = spoti.search(q=s, limit=5, type='album', market='RU')
+    total = []
+    if not res['albums']['items']:
+        result = 'По вашему запросу ничего не нашлось :('
+        total.append(result)
+    for i, elem in enumerate(res['albums']['items']):
+        index = i + 1
+        artist = elem['artists'][0]['name']
+        year = elem['release_date'][:4]
+        name = elem['name']
+        link = elem['external_urls']['spotify']
+        album_type = elem['album_type']
+        # genres = ', '.join(elem['genres'])
+        cover = elem['images'][0]['url']
+        # copyrights = elem['copyrights'][0]['text']
+        result = f'{index}. {artist} - {name} ({year}) ' + \
+                 f'[{album_type}]\n{link}\n{cover}\n'
+        total.append(result)
+    if len(total) == 1:
+        total[0] = total[0][3:]
+    return total
+
+
+def search_for_playlist(text: str):
     pass
 
 
-def search_for_playlist(text):
-    pass
-
-
-# print(*search_for_track('m.,.,/./''),'), sep='\n')
+# print(*search_for_album('there is a hell believe me'), sep='\n')
 # print(*search_for_artist('01gfgetfeDACFA Ggaeg'), sep='\n')
