3e91d1d54e72ac8c5d46da2782bba28fd5532938
feelsbadkot <bond1010a@gmail.com>
2021-04-12 19:43:40 +0500

    Добавил возвращение словаря исполнителя

--- spotify_functions.py
+++ spotify_functions.py
@@ -4,7 +4,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from config import *
-# from pprint import pprint
+from pprint import pprint
 
 
 ccm = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, 
@@ -48,7 +48,7 @@ def search_for_artist(text: str):
     res = spoti.search(q=s, limit=5, type='artist', market='RU')
     total = []
     if not res['artists']['items']:
-        result = 'По вашему запросу ничего не нашлось :('
+        result = '): По вашему запросу ничего не нашлось :('
         total.append(result)
     for i, elem in enumerate(res['artists']['items']):
         index = i + 1
@@ -56,10 +56,11 @@ def search_for_artist(text: str):
         link = elem['external_urls']['spotify']
         genres = ', '.join(elem['genres'])
         image = elem['images'][0]['url']
+        fols= elem['followers']['total']
         if genres:
-            result = f'{index}. {artist} ({genres})\n{link}\n{image}'
+            result = f'{index}. {artist} ({genres}) - {fols} followers\n{link}\n{image}'
         else:
-            result = f'{index}. {artist}\n{link}\n{image}'
+            result = f'{index}. {artist} - {fols}\n{link}\n{image}'
         total.append(result)
     if len(total) == 1:
         total[0] = total[0][3:]
@@ -71,7 +72,7 @@ def search_for_album(text: str):
     res = spoti.search(q=s, limit=5, type='album', market='RU')
     total = []
     if not res['albums']['items']:
-        result = 'По вашему запросу ничего не нашлось :('
+        result = '): По вашему запросу ничего не нашлось :('
         total.append(result)
     for i, elem in enumerate(res['albums']['items']):
         index = i + 1
@@ -96,7 +97,7 @@ def search_for_playlist(text: str):
     res = spoti.search(q=s, limit=3, type='playlist', market='RU')
     total = []
     if not res['playlists']['items']:
-        result = 'По вашему запросу ничего не нашлось :('
+        result = '): По вашему запросу ничего не нашлось :('
         total.append(result)
     for i, elem in enumerate(res['playlists']['items']):
         index = i + 1
@@ -114,7 +115,54 @@ def search_for_playlist(text: str):
     return total
 
 
+def return_artist(text: str):
+    res = spoti.search(q=text, type='artist', market='RU')
+    items = res['artists']['items']
+    if len(items) > 0:
+        return items[0]
+    return None
+
+
+# def return_artists_discography(data: dict): # дата это словарь, который возвращает функия выше
+#     res = spoti.artist_albums(data['id'], album_type='album')
+#     albums = res['items'][:]
+#     total = []
+#     while res['next']:
+#         res = spoti.next(res)
+#         albums.extend(res['items'])
+#     for i, elem in enumerate(albums[::-1]):
+#         index = i + 1
+#         artist = elem['artists'][0]['name']
+#         year = elem['release_date'][:4]
+#         name = elem['name']
+#         link = elem['external_urls']['spotify']
+#         album_type = elem['album_type']
+#         # genres = ', '.join(elem['genres'])
+#         cover = elem['images'][0]['url']
+#         # copyrights = elem['copyrights'][0]['text']
+#         result = f'{index}. {artist} - {name} ({year}) ' + \
+#                  f'[{album_type}]\n{link}\n{cover}\n'
+#         if any(name in i for i in total):
+#             continue
+#         total.append(result)
+#     if len(total) == 1:
+#         total[0] = total[0][3:]
+#     return total
+
+
+
+def return_album_tracks(data: dict):
+    pass
+
+
+def return_artist_top_tracks(data: dict):
+    pass
+
+
+chevelle = return_artist('Alice in chains')
+print(*return_artists_discography(chevelle), sep='\n')
+# pprint(return_artist('Alexisonfire'))
 # print(*search_for_track('suicide season'), sep='\n')
 # print(*search_for_album('there is a hell believe me'), sep='\n')
-# print(*search_for_artist('01gfgetfeDACFA Ggaeg'), sep='\n')
+# print(*search_for_artist('Lil'), sep='\n')
 # print(*search_for_playlist('Полный фреш'), sep='\n')
