ec661f56fccd60f02941e9eb94fd090f6b58418e
feelsbadkot <bond1010a@gmail.com>
2021-04-25 20:01:52 +0500

    Переменные среды

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # в этом файле лежат переменные для совершения GET-запросов
 # их мы получили, зарегистрировав наше приложение на https://developer.spotify.com
+import os
 
-TOKEN = "1770686372:AAH2H3pHCH6ge2QhYwUB0ZvMB4YwInc7Ork"
-SPOTIPY_CLIENT_ID = 'aaa90b245a9f48e9bc277aa6c17c7e21'
-SPOTIPY_CLIENT_SECRET = 'd695fdf4d7064bdb85155733d2585b3e'
+TOKEN = str(os.environ.get('token'))   
+SPOTIPY_CLIENT_ID = str(os.environ.get('client_id')) 
+SPOTIPY_CLIENT_SECRET = str(os.environ.get('client_secret')) 
 SPOTIPY_REDIRECT_URI = 'https://www.spotify.com/us/account/overview/'
 SCOPE = "user-read-playback-state,user-modify-playback-state"
